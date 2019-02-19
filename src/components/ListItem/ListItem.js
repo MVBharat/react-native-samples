a73062d89eeaad3ea46887d56be8ca38eb5b0441
MVBharat <manebharat85@gmail.com>
2019-02-19 14:13:29 +0530

    add image to listItem

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native'
 
 const listItem = (props) => (
     <TouchableOpacity onPress={props.onItemPressed} >
         <View style={styles.listItem}>
+            <Image source={props.placeImage} style={styles.placeImage} resizeMode="cover" />
             <Text>{props.placeName} </Text>
         </View>
     </TouchableOpacity>
@@ -15,6 +16,13 @@ const styles = StyleSheet.create({
         padding: 10,
         backgroundColor: "orange",
         marginBottom: 5,
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    placeImage: {
+        marginRight: 8,
+        height: 30,
+        width: 30
     }
 })
 
